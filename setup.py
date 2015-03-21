328443e4b8d0593fb1358fe8763d7dd65e20ab8e
Goldcap <amadsen@gmail.com>
2015-03-21 19:53:36 -0400

    master: Updated Setup Version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.4',
+    version='1.5',
 
     description='Autogeneration of Ember Models from Django Models',
     #long_description=long_description,
